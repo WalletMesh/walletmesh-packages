e588e1ed8ea139125c152d0f369f272a8949a3da
pavitra-infocusp <pavitra@infocusp.com>
2025-04-07 17:35:01 +0530

    fix build errors

--- core/modal-example/vite.config.ts
+++ core/modal-example/vite.config.ts
@@ -1,22 +1,20 @@
-import { defineConfig, type Plugin, type UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-const config: UserConfig = {
-  plugins: react() as Plugin[],
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-      generateScopedName: '[name]__[local]__[hash:base64:5]',
-    },
-  },
-  optimizeDeps: {
-    include: ['@walletmesh/modal'],
-  },
-};
-
-export default defineConfig(config);
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  //   host: '127.0.0.1',
+  //   port: 5173,
+  // },
+  // css: {
+  //   modules: {
+  //     localsConvention: 'camelCase',
+  //     generateScopedName: '[name]__[local]__[hash:base64:5]',
+  //   },
+  // },
+  // optimizeDeps: {
+  //   include: ['@walletmesh/modal'],
+  // },
+});
