67d746a3bd453e20dc163d6c9ed3a23d7fe5853a
pavitra-infocusp <pavitra@infocusp.com>
2025-04-07 18:39:42 +0530

    fix modal-example

--- core/modal-example/vite.config.ts
+++ core/modal-example/vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '127.0.0.1',
-  //   port: 5173,
-  // },
-  // css: {
-  //   modules: {
-  //     localsConvention: 'camelCase',
-  //     generateScopedName: '[name]__[local]__[hash:base64:5]',
-  //   },
-  // },
-  // optimizeDeps: {
-  //   include: ['@walletmesh/modal'],
-  // },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]__[local]__[hash:base64:5]',
+    },
+  },
+  optimizeDeps: {
+    include: ['@walletmesh/modal'],
+  },
 });
